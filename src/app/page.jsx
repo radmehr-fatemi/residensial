fe43d6d42972c7b0d5b13ef5042367a177c97154
Alireza Fatemi <fa1381138100@gmail.com>
2024-01-20 22:19:37 +0330

    Edited for deploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,7 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
-
 //Component
 import HomePage from "src/components/template/home/HomePage";
 
 const Home = async () => {
-
-  // const session = await getServerSession(authOptions);
-
-  // if (!session) redirect("/signup")
 
   return <HomePage />
 };
